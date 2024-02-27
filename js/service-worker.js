ad01dc8f29eda4dc1b6463aa60347cd5cc998d64
Sebastian Iglesias <sebiglesias@users.noreply.github.com>
2024-02-27 17:16:49 -0300

    test (#3)

---
+++ js/service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'site-static-v1'
+const assetsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/css/bootstrap.css',
+    '/css/button.css',
+    '/css/img.css',
+    '/css/layout.css',
+    '/js/bootstrap.js',
+    '/img/favicon.ico',
+    '/img/me.jpg',
+    '/assets/en/cv.pdf',
+    '/assets/es/cv.pdf'
+]
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                cache.addAll(assetsToCache);
+            })
+    )
+})
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cacheResponse => {
+            return cacheResponse || fetch(event.request);
+        })
+    )
+})
